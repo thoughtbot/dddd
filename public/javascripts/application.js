be8446bbef8fd3054e5063702ee0755bfa587641
Phil LaPier <plapier@thoughtbot.com>
2010-12-01 11:02:38 -0500

    Fuck yeah modernizer111

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,36 +15,4 @@ $(document).ready(function() {
 
     return false;
   });
-    if ($.browser.mozilla) {
-        $('body').addClass("mozilla");
-
-        var versionParts = $.browser.version.split("\.");
-        var version = 0;
-        if (versionParts.length > 0) {
-            version = version + versionParts[0] * 1000000000000;
-        }
-        if (versionParts.length > 1) {
-            version = version + versionParts[1] * 1000000000;
-        }
-        if (versionParts.length > 2) {
-            version = version + versionParts[2] * 1000000;
-        }
-        if (versionParts.length > 3) {
-            version = version + versionParts[3] * 1000;
-        }
-
-        if (version >= 1009002000000) {
-            $('body').addClass("mozilla3-6andAbove");
-        }
-        else {
-            $('body').addClass("mozilla3-5andBelow");
-            var link = $("<link>");
-            link.attr({
-                    type: 'text/css',
-                    rel: 'stylesheet',
-                    href: '/stylesheets/firefox_3.5.css'
-            });
-            $("head").append(link);
-        }
-    }
 });
