e4e9435125fdc485379745eeda063e294ca3e15c
Phil LaPier <plapier@thoughtbot.com>
2010-12-16 17:14:25 -0500

    Updated syntax and removed unused image

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,7 +1,7 @@
 module TwitterHelper
   def twitter_avatar(name, size)
     return "" if name.blank?
-    content_tag :div, :class => "logo" do
+    content_tag :div, :class => "avatar" do
       link_to "http://twitter.com/#{name}" do
         image_tag "http://avatar.statusok.com/#{name}?size=#{size}", :width => "#{size}px", :height => "#{size}px"
       end
