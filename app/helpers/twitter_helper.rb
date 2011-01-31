e9b172ae82512f16ad1a4dc0404e5dfe199f68d4
Dan Croak <dcroak@thoughtbot.com>
2011-01-31 16:28:34 -0500

    display default avatar correctly

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,10 +1,13 @@
 module TwitterHelper
   def twitter_avatar(name, size)
-    content_tag :div, :class => "avatar" do
-      if name.present?
+    if name.present?
+      content_tag :div, :class => "avatar" do
         link_to "http://twitter.com/#{name}" do
           image_tag "http://avatar.statusok.com/#{name}?size=#{size}", :width => "#{size}px", :height => "#{size}px"
         end
+      end
+    else
+      content_tag :div, :class => "avatar default" do
       end
     end
   end
