3a7ac364d9e3acd02ebc167b999a26fdcccc9a35
Dan Croak <dcroak@thoughtbot.com>
2011-02-04 17:28:02 -0500

    flash when rsvp'ing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,6 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_or_create_by_auth(auth)
     session[:user_id] = user.id
-    redirect_to party_url
+    redirect_to party_url, :notice => "Thanks for RSVPing You can change your RSVP below and tell local students about yourself."
   end
 end
