f5ec85355e18b781fe96eca77b5985202a943f84
Dan Croak <dcroak@thoughtbot.com>
2011-02-04 17:15:22 -0500

    making RSVP work, got rid of Ajax form submission for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
   def update
-    if current_user.update_attributes(params[:user])
-      render :partial => 'parties/thanks'
-    else
-      render :partial => 'parties/form'
-    end
+    current_user.update_attributes(params[:user])
+    redirect_to party_path, :notice => "RSVP updated. It's only a party if you make it a party."
   end
 end
